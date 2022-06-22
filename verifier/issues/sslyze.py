a59bae0e31d43e2a44f50b7c1c6e851bf64bb270
Jan-Jaap Korpershoek <jjkorpershoek96@gmail.com>
2022-06-22 16:13:02 +0200

    SSLyze issue

--- verifier/issues/sslyze.py
+++ verifier/issues/sslyze.py
@@ -1,9 +1,28 @@
 from .base import CommandIssue, add_expansion, add_issue
 from functools import cache
 from ..util import SNIP, IssueDoesNotExist, HIGHLIGHT_START, HIGHLIGHT_END
+from collections import defaultdict
 
 class SSLyzeIssue(CommandIssue):
     description = "Run SSLyze"
+    _header = {
+        "en": "SSLyze output:",
+        "nl": "SSLyze output:"
+    }
+    _footer = defaultdict(lambda: "TODO")
+    _template = {
+        "en": """{}
+
+bc.. {}
+
+p. {}""",
+        "nl": """{}
+
+bc.. {}
+
+p. {}""",
+
+    }
 
     @cache
     def run_sslyze(self, host):
@@ -11,6 +30,61 @@ class SSLyzeIssue(CommandIssue):
 
     def verify(self, host):
         result = self.run_sslyze(host)
-        yield result
+        yield self.template.format(self.header, result.rstrip(), self.footer)
 
-add_issue("sslyze", SSLyzeIssue)
+    @property
+    def header(self):
+        return self._header[self.language]
+
+    @property
+    def footer(self):
+        return self._footer[self.language]
+
+
+class SSLyzeTLSVersions(SSLyzeIssue):
+    _footer = {
+        "en": "Outdated TLS versions are supported.",
+        "nl": "Verouderde TLS versies worden ondersteund.",
+    }
+    def postprocess(self, output):
+        output = super().postprocess(output)
+        lines = output.splitlines()
+        found = False
+        new_lines = []
+        for line in lines:
+            if "tls_versions" in line:
+                found = True
+                new_lines.append(HIGHLIGHT_START + line + HIGHLIGHT_END)
+                break
+            else:
+                new_lines.append(line)
+        if not found:
+            raise IssueDoesNotExist
+        return "\n".join(new_lines)
+
+
+class SSLyzeWeakCiphers(SSLyzeIssue):
+    _footer = {
+        "en": "Weak TLS ciphers are supported.",
+        "nl": "Zwakke TLS ciphers worden ondersteund.",
+    }
+    def postprocess(self, output):
+        output = super().postprocess(output)
+        lines = output.splitlines()
+        found = False
+        new_lines = []
+        for line in lines:
+            if "ciphers" in line:
+                found = True
+                line = HIGHLIGHT_START + line + HIGHLIGHT_END
+            new_lines.append(line)
+        if not found:
+            raise IssueDoesNotExist
+        return "\n".join(new_lines)
+
+
+add_issue("sslyze-raw", SSLyzeIssue)
+add_issue("sslyze-versions", SSLyzeTLSVersions)
+add_issue("sslyze-ciphers", SSLyzeWeakCiphers)
+
+add_expansion("sslyze", ["sslyze-versions", "sslyze-ciphers"])
