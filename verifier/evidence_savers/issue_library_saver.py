afa616fecdf6d1bd40d03f5fff42ed4991127ece
Jan-Jaap Korpershoek <jjkorpershoek96@gmail.com>
2022-08-25 16:26:39 +0200

    Update issue library evidence saver with reporter changes

--- verifier/evidence_savers/issue_library_saver.py
+++ verifier/evidence_savers/issue_library_saver.py
@@ -2,16 +2,17 @@ from .evidence_saver import add_evidence_saver, EvidenceSaver as Base
 from ..issues import get_issue
 from os.path import join
 from ..config import config
-from reporter import create_standard_issue, create_evidence
+from reporter import Template, config as reporter_config
 
 class EvidenceSaver(Base):
     def save_evidence(self, evidence):
+        template = Template(reporter_config.get('template'), evidence.lang)
         issue = get_issue(evidence.id, lang=evidence.lang)
         if issue.standard_issue_path:
-            dirname = create_standard_issue(issue.standard_issue_path, output_file=None, do_create_evidence=False)
-            create_evidence(evidence.label or evidence.host, output_dir=dirname, description=evidence.output)
+            dirname = template.report_manager.create_standard_issue(issue.standard_issue_path, output_file=None, do_create_evidence=False)
+            template.report_manager.create_evidence(evidence.label or evidence.host, output_dir=dirname, description=evidence.output)
         else:
-            create_evidence(evidence.label or evidence.host, description=evidence.output)
+            template.report_manager.create_evidence(evidence.label or evidence.host, description=evidence.output)
 
 
 add_evidence_saver("issue-library", EvidenceSaver())
