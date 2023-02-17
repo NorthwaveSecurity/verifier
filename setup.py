7b394af514675cc2daa522f0444bdf27dc422eea
Jan-Jaap Korpershoek <jjkorpershoek96@gmail.com>
2023-02-17 11:18:15 +0100

    Add a default section for installing the default extra's

--- setup.py
+++ setup.py
@@ -2,12 +2,20 @@
 from distutils.core import setup
 from setuptools import find_packages
 
+dradis = ["dradis-api @ git+https://github.com/NorthwaveSecurity/dradis-api.git"]
+reporter = ['reporter @ git+https://github.com/JJK96/reporter.git']
+cookie_flags_browser = ['browser_cookie3']
+sslyze = ['sslyze']
+
+default = dradis + cookie_flags_browser + sslyze
+
+
 setup(name='Verifier',
       version='1.0',
       description='Automatically verify security issues',
       author='Jan-Jaap Korpershoek',
       author_email='janjaap.korpershoek@northwave.nl',
-      install_requires=['argcomplete', 'requests'],
+      install_requires=['argcomplete', 'requests', 'cachetools'],
       entry_points={
         'console_scripts': ['dradis_curl=dradis_curl:main', 'verifier=verifier.verifier:main'],
       },
@@ -15,8 +23,10 @@ setup(name='Verifier',
       py_modules=["dradis_curl"],
       scripts=['start_test'],
       extras_require={
-          "dradis": "dradis-api @ git+https://github.com/NorthwaveSecurity/dradis-api.git",
-          'reporter': 'reporter @ git+https://github.com/JJK96/reporter.git',
-          'cookie-flags-browser': 'browser_cookie3',
+          "default": default,
+          "dradis": dradis,
+          'reporter': reporter,
+          'cookie-flags-browser': cookie_flags_browser,
+          'sslyze': sslyze
       }
       )
