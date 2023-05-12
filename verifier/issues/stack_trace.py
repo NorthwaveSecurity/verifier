cfbe550fb979ec25bce673dfa2869408a95c9a3d
Jan-Jaap Korpershoek <jjkorpershoek96@gmail.com>
2023-05-12 15:56:34 +0200

    Extend stacktrace issue with new format

--- verifier/issues/stack_trace.py
+++ verifier/issues/stack_trace.py
@@ -9,7 +9,7 @@ class StackTrace(DradisCurlIssue):
     stacktrace_started = False
 
     def is_stacktrace_line(self, line):
-        start_regex = re.compile(r"line \d+|Exception in thread|stack-error")
+        start_regex = re.compile(r"line \d+|Exception in thread|stack-error|Caused by:")
         stacktrace_regex = re.compile(r"at .*?\(|[\w\.\d]+\([\w\d]+\.java:\d+\)")
         if start_regex.search(line):
             self.stacktrace_started = True
